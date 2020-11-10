d64a4290c85dc6c26c7a382ecb61a75523d1785c
Masatoshi Hayashi <m@literalice.com>
2020-11-10 16:23:32 +0900

    vuex

--- investier-frontend/src/main.js
+++ investier-frontend/src/main.js
@@ -5,6 +5,17 @@ import 'uikit/dist/css/uikit.min.css'
 UIkit.use(Icons)
 
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state () {
+      return {
+        count: 1
+      }
+    }
+  })
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
+app.use(store)
